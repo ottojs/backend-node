9b19cb72289ae76e4771c0cd138d14baf5269482
Ryan Lelek <ryanlelek@gmail.com>
2024-06-03 20:53:28 -0500

    Add: Local environment helpers

---
+++ env.dev.ps1
@@ -0,0 +1,5 @@
+#!/usr/bin/env pwsh
+
+$env:NODE_ENV = "development";
+$env:LISTEN = "127.0.0.1";
+$env:PORT = "8080";
---
+++ env.dev.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export NODE_ENV="development";
+export LISTEN="127.0.0.1";
+export PORT="8080";
