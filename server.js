7b3c2e2e09ac46bbd6696027e5e134715942aa52
Ryan Lelek <ryanlelek@gmail.com>
2024-06-03 22:06:13 -0500

    Edit: server.js to use config and handle uncaught errors

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 // Modules
+import config from './src/lib/config.js';
 import app from './src/index.js';
 
+// NOTE: This file is kept simple intentionally
+//       Please don't add anything here unless
+//       it directly relates to the HTTP server
+
 // HTTP Server
-app.listen(8080, '127.0.0.1', function () {
-	console.log(`[HTTP:SERVER] OK - Listening on 8080`);
+app.listen(config.PORT, config.LISTEN, function () {
+	console.log(
+		`[HTTP:SERVER] OK - Listening on ${config.LISTEN}:${config.PORT}`
+	);
 });
+
+// Handle exceptions and shutdown
+process
+	.on('unhandledRejection', (reason, p) => {
+		console.error(reason, 'Unhandled Rejection at Promise', p);
+		process.exit(1);
+	})
+	.on('uncaughtException', (err) => {
+		console.error(err, 'Uncaught Exception thrown');
+		process.exit(1);
+	});
