c88b695330413c82e202a49447784de0895cdce6
Ryan Lelek <ryanlelek@gmail.com>
2024-06-05 18:43:37 -0500

    Add: SQL unique index on account.uuid

--- src/sql/migrations/0003_accounts.cjs
+++ src/sql/migrations/0003_accounts.cjs
@@ -3,35 +3,48 @@
 module.exports = {
 	async up(queryInterface, Sequelize) {
 		// Accounts
-		await queryInterface.createTable('accounts', {
-			id: {
-				type: Sequelize.INTEGER,
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
+		await queryInterface.createTable(
+			'accounts',
+			{
+				id: {
+					type: Sequelize.INTEGER,
+					allowNull: false,
+					autoIncrement: true,
+					primaryKey: true,
+				},
+				uuid: {
+					type: Sequelize.UUID,
+					unique: true,
+					allowNull: false,
+				},
+				name: {
+					type: Sequelize.STRING,
+					allowNull: false,
+				},
+				created_at: {
+					type: Sequelize.DATE,
+					allowNull: false,
+				},
+				updated_at: {
+					type: Sequelize.DATE,
+					allowNull: false,
+				},
+				deleted_at: {
+					type: Sequelize.DATE,
+					allowNull: true,
+				},
 			},
-			uuid: {
-				type: Sequelize.UUID,
-				unique: true,
-				allowNull: false,
-			},
-			name: {
-				type: Sequelize.STRING,
-				allowNull: false,
-			},
-			created_at: {
-				type: Sequelize.DATE,
-				allowNull: false,
-			},
-			updated_at: {
-				type: Sequelize.DATE,
-				allowNull: false,
-			},
-			deleted_at: {
-				type: Sequelize.DATE,
-				allowNull: true,
-			},
-		});
+			{
+				// TODO: Does a unique index get created by specifying "unique" above?
+				indexes: [
+					{
+						name: 'idx_accounts_uniq_uuid',
+						unique: true,
+						fields: ['uuid'],
+					},
+				],
+			}
+		);
 	},
 	async down(queryInterface) {
 		await queryInterface.dropTable('accounts');
