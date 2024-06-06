f66000ce43d304b670cb588951c80cc885531418
Ryan Lelek <ryanlelek@gmail.com>
2024-06-05 19:29:39 -0500

    Add: Error handling options

--- src/mw/error_handler.mw.js
+++ src/mw/error_handler.mw.js
@@ -1,5 +1,36 @@
+// Modules
+import debug from 'debug';
+const log = debug('app:mw:error_handler');
+
 function mw_error_handler(error, req, res, next) {
-	if (error.message === 'not_found') {
+	if (error.message === 'bad_request') {
+		res.status(400);
+		res.json({
+			status: 'error',
+			error: {
+				code: 400,
+				message: 'bad request',
+			},
+		});
+	} else if (error.message === 'unauthorized') {
+		res.status(401);
+		res.json({
+			status: 'error',
+			error: {
+				code: 401,
+				message: 'unauthorized',
+			},
+		});
+	} else if (error.message === 'forbidden') {
+		res.status(403);
+		res.json({
+			status: 'error',
+			error: {
+				code: 403,
+				message: 'forbidden',
+			},
+		});
+	} else if (error.message === 'not_found') {
 		res.status(404);
 		res.json({
 			status: 'error',
@@ -10,6 +41,7 @@ function mw_error_handler(error, req, res, next) {
 			},
 		});
 	} else {
+		log('[ERROR HANDLER UNEXPECTED] 500', error);
 		res.status(500);
 		res.json({
 			status: 'error',
--- test/unit/mw/error_handler.mw.test.js
+++ test/unit/mw/error_handler.mw.test.js
@@ -2,8 +2,8 @@
 import mw_error_handler from '../../../src/mw/error_handler.mw.js';
 
 describe('mw_error_handler()', () => {
-	describe('when error is unexpected', () => {
-		it('should set status to 500', (done) => {
+	describe('when error is bad_request', () => {
+		it('should set status to 400', (done) => {
 			let status;
 			const res = {
 				status: (int) => {
@@ -11,12 +11,12 @@ describe('mw_error_handler()', () => {
 				},
 				json: () => {},
 			};
-			mw_error_handler(new Error('unexpected'), {}, res, () => {
-				expect(status).toEqual(500);
+			mw_error_handler(new Error('bad_request'), {}, res, () => {
+				expect(status).toEqual(400);
 				done();
 			});
 		});
-		it('should set json to "internal server error"', (done) => {
+		it('should set json to "bad request"', (done) => {
 			let json;
 			const res = {
 				status: () => {},
@@ -24,10 +24,13 @@ describe('mw_error_handler()', () => {
 					json = obj;
 				},
 			};
-			mw_error_handler(new Error('unexpected'), {}, res, () => {
+			mw_error_handler(new Error('bad_request'), {}, res, () => {
 				expect(json).toEqual({
 					status: 'error',
-					error: { code: 500, message: 'internal server error' },
+					error: {
+						code: 400,
+						message: 'bad request',
+					},
 				});
 				done();
 			});
@@ -37,7 +40,89 @@ describe('mw_error_handler()', () => {
 				status: () => {},
 				json: () => {},
 			};
-			mw_error_handler(new Error('unexpected'), {}, res, done);
+			mw_error_handler(new Error('bad_request'), {}, res, done);
+		});
+	});
+	describe('when error is unauthorized', () => {
+		it('should set status to 401', (done) => {
+			let status;
+			const res = {
+				status: (int) => {
+					status = int;
+				},
+				json: () => {},
+			};
+			mw_error_handler(new Error('unauthorized'), {}, res, () => {
+				expect(status).toEqual(401);
+				done();
+			});
+		});
+		it('should set json to "unauthorized"', (done) => {
+			let json;
+			const res = {
+				status: () => {},
+				json: (obj) => {
+					json = obj;
+				},
+			};
+			mw_error_handler(new Error('unauthorized'), {}, res, () => {
+				expect(json).toEqual({
+					status: 'error',
+					error: {
+						code: 401,
+						message: 'unauthorized',
+					},
+				});
+				done();
+			});
+		});
+		it('should call next', (done) => {
+			const res = {
+				status: () => {},
+				json: () => {},
+			};
+			mw_error_handler(new Error('unauthorized'), {}, res, done);
+		});
+	});
+	describe('when error is forbidden', () => {
+		it('should set status to 403', (done) => {
+			let status;
+			const res = {
+				status: (int) => {
+					status = int;
+				},
+				json: () => {},
+			};
+			mw_error_handler(new Error('forbidden'), {}, res, () => {
+				expect(status).toEqual(403);
+				done();
+			});
+		});
+		it('should set json to "forbidden"', (done) => {
+			let json;
+			const res = {
+				status: () => {},
+				json: (obj) => {
+					json = obj;
+				},
+			};
+			mw_error_handler(new Error('forbidden'), {}, res, () => {
+				expect(json).toEqual({
+					status: 'error',
+					error: {
+						code: 403,
+						message: 'forbidden',
+					},
+				});
+				done();
+			});
+		});
+		it('should call next', (done) => {
+			const res = {
+				status: () => {},
+				json: () => {},
+			};
+			mw_error_handler(new Error('forbidden'), {}, res, done);
 		});
 	});
 	describe('when error is not_found', () => {
@@ -87,6 +172,44 @@ describe('mw_error_handler()', () => {
 				json: () => {},
 			};
 			mw_error_handler(new Error('not_found'), {}, res, done);
+		});
+	});
+	describe('when error is unexpected', () => {
+		it('should set status to 500', (done) => {
+			let status;
+			const res = {
+				status: (int) => {
+					status = int;
+				},
+				json: () => {},
+			};
+			mw_error_handler(new Error('unexpected'), {}, res, () => {
+				expect(status).toEqual(500);
+				done();
+			});
+		});
+		it('should set json to "internal server error"', (done) => {
+			let json;
+			const res = {
+				status: () => {},
+				json: (obj) => {
+					json = obj;
+				},
+			};
+			mw_error_handler(new Error('unexpected'), {}, res, () => {
+				expect(json).toEqual({
+					status: 'error',
+					error: { code: 500, message: 'internal server error' },
+				});
+				done();
+			});
+		});
+		it('should call next', (done) => {
+			const res = {
+				status: () => {},
+				json: () => {},
+			};
+			mw_error_handler(new Error('unexpected'), {}, res, done);
 		});
 	});
 });
